cfeaba3fd2fae44ef9d20bb9c77da1fd9fa665b4
takuya178 <takuya.u1005@gmail.com>
2021-12-26 16:08:06 +0900

    ライバルのチュートリアル追加

--- rival.rb
+++ rival.rb
@@ -1,3 +1,19 @@
 class Rival
+  attr_reader :name, :money, :pokemon
+  def initialize(**params)
+    @name = params[:name]
+    @money = params[:money]
+    @pokemon = params[:pokemon]
+  end
 
+  def rival_pokemon(character)
+    if "#{character.pokemon.name}" == "ヒトカゲ"
+      puts "なら#{@name}はゼニガメじゃな"
+    elsif "#{character.pokemon.name}" == "ゼニガメ"
+      puts "なら#{@name}はフシギダネじゃな"
+    elsif "#{character.pokemon.name}" == "フシギダネ"
+      puts "なら#{@name}はヒトカゲじゃな"
+    end
+
+  end
 end
